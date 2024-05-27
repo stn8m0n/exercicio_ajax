19b5d94f722a53595d67be8e33b808dd7109066d
Maicon Santana <gti.maicon@gmail.com>
2024-05-27 05:52:54 -0300

    startAtt

--- ajax.js
+++ ajax.js
@@ -1,25 +1,25 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-            const nameElement = document.querySelector('#name');
-            const usernameElement = document.querySelector('#username');
-            const avatarElement = document.querySelector('#avatar');
-            const reposElement = document.querySelector('#repos');
-            const followersElement = document.querySelector('#followers');
-            const followingElement = document.querySelector('#following');
-            const linkElement = document.querySelector('#link');
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
 
-            fetch('https://api.github.com/users/stn8m0n')
-                .then(function(res){
-                    return res.json();
-                })
-                .then(function(json){
-                    nameElement.innerText = json.name;
-                    usernameElement.innerText = json.login;
-                    avatarElement.src = json.avatar_url;
-                    followersElement.innerText = json.followers;
-                    followingElement.innerText = json.following;
-                    reposElement.innerText = json.public_repos;
-                    linkElement.href = json.html_url;
-                })
+    fetch('https://api.github.com/users/stn8m0n')
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json){
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            reposElement.innerText = json.public_repos;
+            linkElement.href = json.html_url;
+        })
 
- })
+})
